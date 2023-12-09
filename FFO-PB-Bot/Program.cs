6088aefcbfa6fc01d6ab10d5dcace64a3573b62c
Robotwars1 <magnus.carlsson4@gmail.com>
2023-12-09 12:38:57 +0100

    Feat. Basics for working bot

--- FFO-PB-Bot/Program.cs
+++ FFO-PB-Bot/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private DiscordSocketClient Client;
+
+    public async Task MainAsync()
+    {
+        Client = new DiscordSocketClient();
+
+        // Setup logging
+        Client.Log += Log;
+
+        // Read bot-token
+        // DO NOT MAKE TOKEN PUBLIC
+        var token = File.ReadAllText("Token.txt");
+
+        // Start bot
+        await Client.LoginAsync(TokenType.Bot, token);
+        await Client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
